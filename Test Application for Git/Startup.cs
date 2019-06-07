f2b71caf25f14b25fedfad7bc360dac0fc0caa5e
trentg@ppai.org <trentg@ppai.org>
2019-06-07 16:33:32 -0500

    Testing

--- Test Application for Git/Startup.cs
+++ Test Application for Git/Startup.cs
@@ -64,3 +64,4 @@ namespace Test_Application_for_Git
 // This comment was added for Feature 120 : Trent
 // This comment was added right on the dev branch as a test.
 // This comment was added on dev with Vern
+// This comment was added on dev with Vern 2
